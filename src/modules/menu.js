5d3b4519974dc6626b293153ca87d55df017b494
Nata2401 <kuzyan.rd@yandex.ru>
2021-12-28 13:25:03 +0300

    Исправлены недочеты в задании 22

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -16,9 +16,9 @@ const menu = () => {
     if (e.target.closest(".close-btn") || e.target.closest(".menu")) {
       e.preventDefault();
       toggleMenu();
-    } else if (e.target.closest("a")) {
+    } else if (e.target.closest(".active-menu>ul>li>a")) {
       e.preventDefault();
-      goToAncor(e.target.closest("a"));
+      goToAncor(e.target.closest(".active-menu>ul>li>a"));
       toggleMenu();
     } else if (e.target !== menu && menu.classList.contains("active-menu")) {
       toggleMenu();
--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,12 +1,11 @@
 const scrollSmooth = () => {
-  const toServiceBtn = document.querySelector(".main>a");
+  const toServiceBtn = document.querySelector("a");
 
   toServiceBtn.addEventListener("click", (e) => {
     e.preventDefault();
     const blockID = toServiceBtn.getAttribute("href");
     document.querySelector("" + blockID).scrollIntoView({
       behavior: "smooth",
-      block: "start",
     });
   });
 };
