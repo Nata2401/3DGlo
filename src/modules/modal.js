aea6f98473bd648c1b59c5c74b0e17d75edcd0c9
Nata2401 <kuzyan.rd@yandex.ru>
2022-01-05 18:23:25 +0300

    Выполнено задание 25

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,69 +1,46 @@
-"use stcrict";
+import { animate } from "./helpers";
+
 const modal = () => {
   const buttons = document.querySelectorAll(".popup-btn");
   const modal = document.querySelector(".popup");
   const modalContent = modal.querySelector(".popup-content");
-  const modalClose = modal.querySelector(".popup-close");
 
-  buttons.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const topOpen = () => {
-        modal.style.display = "block";
-        modal.style.top = 0;
-
-        let idInterval;
-        let count = 0;
-
-        const appear = () => {
-          count++;
-          idInterval = requestAnimationFrame(appear);
-          if (count <= 15) {
-            modalContent.style.top = count * 10 + "px";
-          } else {
-            cancelAnimationFrame(idInterval);
-          }
-        };
-        idInterval = requestAnimationFrame(appear);
-      };
-
-      if (window.innerWidth > 768) {
-        topOpen();
-      } else {
-        modal.style.display = "block";
-      }
+  const showModal = () => {
+    modal.style.display = "block";
+    animate({
+      duration: 400,
+      timing(timeFraction) {
+        return timeFraction;
+      },
+      draw(progress) {
+        modalContent.style.transform = `scale(${progress})`;
+        modal.style.opacity = progress;
+      },
     });
+  };
 
-    modalClose.addEventListener("click", () => {
-      const topClose = () => {
-        let idInterval;
-        let count = 15;
-
-        const close = () => {
-          if (count >= 0) {
-            count--;
-            modalContent.style.top = count * 10 + "px";
-            idInterval = requestAnimationFrame(close);
-          } else {
-            cancelAnimationFrame(idInterval);
-            modal.style.display = "none";
-          }
-        };
-        idInterval = requestAnimationFrame(close);
-      };
-
-      if (window.innerWidth > 768) {
-        topClose();
-      } else {
-        modal.style.display = "none";
-      }
+  const closeModal = () => {
+    animate({
+      duration: 400,
+      timing(timeFraction) {
+        return timeFraction;
+      },
+      draw(progress) {
+        modalContent.style.transform = `scale(${1 - progress})`;
+        modal.style.opacity = 1 - progress;
+      },
     });
-  });
+    setTimeout(() => (modal.style.display = "none"), 400);
+  };
+
+  buttons.forEach((button) => button.addEventListener("click", showModal));
+
   modal.addEventListener("click", (e) => {
     if (
       !e.target.closest(".popup-content") ||
       e.target.classList.contains("popup-close")
     ) {
-      modal.style.display = "none";
+      closeModal();
     }
   });
 };
