af555b514031195e4379a411027f7a03c49c118b
Nata2401 <kuzyan.rd@yandex.ru>
2022-01-06 16:28:41 +0300

    Накрутка стоимости в калькуляторе через animate

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,3 +1,5 @@
+import { animate } from "./helpers";
+
 const calc = (price = 100) => {
   const calcBlock = document.querySelector(".calc-block");
   const calcType = document.querySelector(".calc-type");
@@ -32,6 +34,16 @@ const calc = (price = 100) => {
     }
 
     total.textContent = totalValue;
+
+    animate({
+      duration: 350,
+      timing(timeFraction) {
+        return timeFraction;
+      },
+      draw(progress) {
+        total.textContent = Math.round(totalValue * progress);
+      },
+    });
   };
 
   calcBlock.addEventListener("input", (e) => {
